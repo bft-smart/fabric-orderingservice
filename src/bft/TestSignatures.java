c0f82c057f569a8ba00da54b07e2e02fd0ae79df
João Sousa <jcs@lasige.di.fc.ul.pt>
2017-04-18 00:46:06 +0100

    Changed executor yet again

--- src/bft/TestSignatures.java
+++ src/bft/TestSignatures.java
@@ -78,11 +78,13 @@ public class TestSignatures {
         TestSignatures.crypto.init();
         TestSignatures.rand = new Random(System.nanoTime());
         //TestSignatures.executor = Executors.newFixedThreadPool(Integer.parseInt(args[0]));
-        TestSignatures.executor = Executors.newCachedThreadPool((Runnable r) -> {
+       
+        /*TestSignatures.executor = Executors.newCachedThreadPool((Runnable r) -> {
             Thread t = new Thread(r);
             t.setPriority(Thread.MAX_PRIORITY);
             return t;
-        });
+        });*/
+        TestSignatures.executor = Executors.newWorkStealingPool();
         TestSignatures.privKey = getPemPrivateKey(args[0]);
         parseCertificate(args[1]);
         TestSignatures.ident = getSerializedIdentity();
