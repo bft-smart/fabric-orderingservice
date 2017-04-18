5fb44c5b19447c0a6ab442c57ca7dfa33357b1b0
João Sousa <jcs@lasige.di.fc.ul.pt>
2017-04-18 04:27:01 +0100

    ...

--- src/bft/TestSignatures.java
+++ src/bft/TestSignatures.java
@@ -85,17 +85,17 @@ public class TestSignatures {
         TestSignatures.crypto = new CryptoPrimitives();
         TestSignatures.crypto.init();
         TestSignatures.rand = new Random(System.nanoTime());
-        TestSignatures.executor = Executors.newFixedThreadPool(Integer.parseInt(args[3]), (Runnable r) -> {
-            Thread t = new Thread(r);
-            t.setPriority(Thread.MAX_PRIORITY);
-            return t;
-        });
-       
-        /*TestSignatures.executor = Executors.newCachedThreadPool((Runnable r) -> {
+        /*TestSignatures.executor = Executors.newFixedThreadPool(Integer.parseInt(args[3]), (Runnable r) -> {
             Thread t = new Thread(r);
             t.setPriority(Thread.MAX_PRIORITY);
             return t;
         });*/
+       
+        TestSignatures.executor = Executors.newCachedThreadPool((Runnable r) -> {
+            Thread t = new Thread(r);
+            t.setPriority(Thread.MAX_PRIORITY);
+            return t;
+        });
         //TestSignatures.executor = Executors.newWorkStealingPool();
         
         TestSignatures.privKey = getPemPrivateKey(args[0]);
