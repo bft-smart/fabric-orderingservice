551c2d07ac5ca09a5badab773011c12810f72315
João Sousa <jcs@lasige.di.fc.ul.pt>
2017-04-18 05:38:34 +0100

    ...

--- src/bft/TestSignatures.java
+++ src/bft/TestSignatures.java
@@ -104,7 +104,7 @@ public class TestSignatures {
         
         TestSignatures.twoSigs =  Boolean.parseBoolean(args[2]);
         
-        interval = Integer.parseInt(args[4]);
+        interval = 100 * Integer.parseInt(args[4]);
         
         //Generate pool of batches
         System.out.print("Generating " + NUM_BATCHES + " batches with " + BATCH_SIZE + " envelopes each... ");
