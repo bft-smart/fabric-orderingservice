9bc9c1c45f1e06ec29f46a15174677e4ae1b1670
João Sousa <jcs@lasige.di.fc.ul.pt>
2017-04-20 15:00:05 +0100

    ...

--- launchTimestampClients.sh
+++ launchTimestampClients.sh
@@ -9,6 +9,6 @@ cd $PATH
 for ((i=0;i<CLI;i++)); do
 
 echo 'Launching client #'$i 
-CMD=""$PATH"orderer/sample_clients/broadcast_timestamp/broadcast_timestamp --messages "$NUM""
+CMD=""$PATH"orderer/sample_clients/broadcast_timestamp/broadcast_timestamp --secure --messages "$NUM""
 $CMD &
 done
