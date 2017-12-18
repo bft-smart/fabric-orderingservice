04153c2801dbda62c0c76428d92531637ec205df
jcs47 <jcs@lasige.di.fc.ul.pt>
2017-12-18 18:36:20 +0000

    Delete launch4Replicas.sh

--- launch4Replicas.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gnome-terminal --tab --working-directory=$(pwd)  -e "bash -c './startReplica.sh 0 10 /home/joao/gocode/src/github.com/hyperledger/fabric/sampleconfig/msp/signcerts/peer.pem /home/joao/gocode/src/github.com/hyperledger/fabric/sampleconfig/msp/keystore/key.pem 1000,2000; bash'"   \
-      --tab --working-directory=$(pwd) -e "bash -c './startReplica.sh 1 10 /home/joao/gocode/src/github.com/hyperledger/fabric/sampleconfig/msp/signcerts/peer.pem /home/joao/gocode/src/github.com/hyperledger/fabric/sampleconfig/msp/keystore/key.pem 1000,2000; bash'"	\
-      --tab --working-directory=$(pwd) -e "bash -c './startReplica.sh 2 10 /home/joao/gocode/src/github.com/hyperledger/fabric/sampleconfig/msp/signcerts/peer.pem /home/joao/gocode/src/github.com/hyperledger/fabric/sampleconfig/msp/keystore/key.pem 1000,2000; bash'"	\
-      --tab --working-directory=$(pwd) -e "bash -c './startReplica.sh 3 10 /home/joao/gocode/src/github.com/hyperledger/fabric/sampleconfig/msp/signcerts/peer.pem /home/joao/gocode/src/github.com/hyperledger/fabric/sampleconfig/msp/keystore/key.pem 1000,2000; bash'"
