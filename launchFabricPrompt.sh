4dfe8bd78b6fbc469b08c57df921c961c7ca9ac1
jcs47 <jcs@lasige.di.fc.ul.pt>
2017-12-18 18:36:36 +0000

    Delete launchFabricPrompt.sh

--- launchFabricPrompt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gnome-terminal --tab --working-directory=$GOPATH'/src/github.com/hyperledger/fabric/'  -e "bash -c 'bash'"   \
-      --tab --working-directory=$GOPATH'/src/github.com/hyperledger/fabric/orderer/sample_clients/deliver_stdout/' -e "bash -c 'bash'"	\
-      --tab --working-directory=$GOPATH'/src/github.com/hyperledger/fabric/orderer/sample_clients/broadcast_timestamp/' -e "bash -c 'bash'"
