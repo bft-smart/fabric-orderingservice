d1f294a8bd06ec8878f47ab242baa3a0cc5b843e
João Sousa <jcs@lasige.di.fc.ul.pt>
2018-07-29 20:57:28 +0100

    D'oh!

--- docker_images/prepare_frontend.sh
+++ docker_images/prepare_frontend.sh
@@ -11,9 +11,9 @@ function main () {
 	#	exit 1
 	#fi
 
-	if [[ "$(docker images -q bftsmart/fabric-orderingnode 2> /dev/null)" == "" ]]; then
+	if [[ "$(docker images -q bftsmart/fabric-frontend 2> /dev/null)" == "" ]]; then
 
-		echo "bftsmart/fabric-orderingnode missing, either pull it from repository or compile it"
+		echo "bftsmart/fabric-frontend missing, either pull it from repository or compile it"
 		exit 1
 		
 	fi
